50d5f743b18c7c580785350950ec109953e8df67
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2023-09-23 18:27:11 -0500

    cartontext Functions

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,12 +1,16 @@
-import carrito from "./carrito-de-compras.png"
+import carrito from "./carrito-de-compras.png";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
 
 const CartWidget = () => {
-  return (
-    <div className='self-center flex gap-2 bg-blue-500 p-2 rounded-xl'>
-        <img src={carrito} alt="imagen carrito" className="w-10" />
-        <p>5</p>
-    </div>
-  )
-}
+  const { totalProductsInWidget } = useContext(CartContext);
 
-export default CartWidget
+  return (
+    <div className="self-center flex gap-2 bg-white p-2 rounded-xl">
+      <img src={carrito} alt="imagen carrito" className="w-10" />
+      <p className="text-2xl my-1 text-blue-800">{totalProductsInWidget}</p>
+    </div>
+  );
+};
+
+export default CartWidget;
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -4,27 +4,32 @@ export const CartContext = createContext([]);
 
 export const CartProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const totalProductsInWidget = products.reduce(
+    (acc, item) => acc + item.amount,
+    0
+  );
 
-  const addItem = (producto, amount) =>
-    setProducts((prev) => [...prev, { ...producto, amount }]);
+  // const addItem = (producto, amount) =>
+  //   setProducts((prev) => [...prev, { ...producto, amount }]);
 
-  // const addItem = (producto, amount) => {
-  //   const yaExiste = products.some((item) => item.id === producto.id);
+  const isInCart = (id) => products.some((item) => item.id === id);
 
-  //   if (!yaExiste) {
-  //     setProducts((prev) => [...prev, { ...producto, amount }]);
-  //   } else {
-  //     const productosActualizados = products.map((item) => {
-  //       if (item.id === producto.id)
-  //         return {
-  //           ...item,
-  //           amount: item.amount + amount,
-  //         };
-  //       else return item;
-  //     });
-  //     setProducts(productosActualizados);
-  //   }
-  // };
+  const addItem = (producto, amount) => {
+    // const yaExiste = products.some((item) => item.id === producto.id );
+    if (!isInCart(producto.id)) {
+      setProducts((prev) => [...prev, { ...producto, amount }]);
+    } else {
+      const productosActualizados = products.map((item) => {
+        if (item.id === producto.id)
+          return {
+            ...item,
+            amount: item.amount + amount,
+          };
+        else return item;
+      });
+      setProducts(productosActualizados);
+    }
+  };
 
   const removeItem = (id) => {
     const productsAfterRemovedItem = products.filter((item) => item.id !== id);
@@ -39,7 +44,9 @@ export const CartProvider = ({ children }) => {
   //   const [numClicks, setnumClicks] = useState(1);
 
   return (
-    <CartContext.Provider value={{ addItem, removeItem, clear }}>
+    <CartContext.Provider
+      value={{ addItem, removeItem, clear, totalProductsInWidget }}
+    >
       {children}
     </CartContext.Provider>
   );
